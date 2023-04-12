6b17f7d586265efeaf467224ce9be70aef7db552
Lyubchenko Valentin <valentin.lyubchenko@extern.a1.at>
2023-04-12 15:03:42 +0300

    fix convert

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,12 +1,15 @@
 package org.example.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 public class Student {
-    String name;
-    String secondname;
-    List<Integer> educ;
+    private String name;
+    private String secondname;
+    private List<Integer> educ;
 }
